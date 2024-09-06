d8e1b2bcafa68b1dd3d79671799ec68b11ab0729
ruriyoshinova <github@ruriyoshinova.dev>
2024-09-06 21:56:13 +0800

    🦄 refactor: Scraper Restructure

--- app/Spiders/VidstreamVideoSpider.php
+++ app/Spiders/VidstreamVideoSpider.php
@@ -53,26 +53,6 @@ class VidstreamVideoSpider extends BasicSpider
         VidstreamItemProcessor::class,
     ];
 
-    /**
-     * --------------------------------------------------------------------------
-     * # Video episode page parser
-     * --------------------------------------------------------------------------
-     *
-     * ### Returns
-     * 1. anime
-     * > eg; No Game No Life
-     *
-     * 2. brief (description)
-     *
-     * 3. related
-     * > - title
-     * > - episode_id
-     * > - date_added
-     * > - splash
-     *
-     * @param \RoachPHP\Http\Response $response
-     * @return \Generator
-     */
     public function parse(Response $response): Generator
     {
         $iframe = $response->filter('.play-video iframe[src]');
@@ -80,54 +60,21 @@ class VidstreamVideoSpider extends BasicSpider
         if (!$iframe->getNode(0)) {
             yield $this->item(['errors' => "Didn't find anything :<"]);
         } else {
-            $related =  $response->filter('.listing.lists li')->each(
-                fn (Crawler $node) => [
-                        'title'         =>  urldecode($node->filter('.name')->text()),
-                        'episode_id'    =>  explode('/videos/', urldecode($node->filter('a')->first()->attr('href')))[1],
-                        'date_added'    =>  $node->filter('.meta .date')->text(),
-                        'splash'        =>  $node->filter('.img .picture img')->attr('src'),
-                    ]
-                );
-            yield $this->request('GET', $iframe->attr('src'), 'parseIframe');
             yield $this->item([
-                'anime'             => $response->filter('.video-details .date')->text(),
-                'brief'             => $response->filter('.video-details .post-entry')->text(),
-                'related'           => $related,
-                ]
-            );
+                'related' => $response->filter('.listing.lists li')->each(
+                    fn(Crawler $node) => [
+                        explode('/videos/', urldecode($node->filter('a')->first()->attr('href')))[1] => [
+                            'title' => urldecode($node->filter('.name')->text()),
+                            'date_added' => $node->filter('.meta .date')->text(),
+                            'splash' => $node->filter('.img .picture img')->attr('src'),
+                        ]
+                    ]
+                )
+            ]);
+            yield $this->request('GET', $iframe->attr('src'), 'parseIframe');
         }
     }
 
-    /**
-     * --------------------------------------------------------------------------
-     * # Video Iframe parser
-     * --------------------------------------------------------------------------
-     *
-     * This is ran after we crawl through the main episode page
-     * The difference is that this retrieves the video itself
-     *
-     * ### Returns
-     * 1. title
-     * > eg; No Game No Life Episode 1
-     *
-     * 2. episode_id
-     * > eg; no-game-no-life-episode-1
-     *
-     * 3. video_id
-     * > NDI3NzY=
-     *
-     * 4. type
-     * > eg; SUB/DUB
-     *
-     * 5. token
-     * > This is an **encrypted token** which is in Base64 format
-     * > that needs to be decoded using AES decryption.
-     * > ```aes->decrypt(base64_decode(token))```
-     *
-     *
-     * @param \RoachPHP\Http\Response $response
-     * @return \Generator
-     */
     public function parseIframe(Response $response): Generator
     {
         $ajax = new Ajax(
@@ -143,15 +90,11 @@ class VidstreamVideoSpider extends BasicSpider
         $episode_id = preg_replace('/[^a-z\s\d]/', '', strtolower(urldecode($query['title'])));
 
         yield $this->item([
-            'title'             =>  urldecode(str_replace('+', ' ', $response->filter('#title')->attr('value'))),
-            'episode_id'        =>  str_replace(' ', '-', $episode_id),
-            'video_id'          =>  $response->filter('#id')->attr('value'),
-            'type'              =>  $response->filter('#typesub')->attr('value'),
-            // 'download_uri'      => "https://s3taku.com/download?id=$token",
-            // 'token'             =>  $token,
-            // 'encryption_key'    =>  explode('container-', $response->filter("body[class^='container-']")->attr('class'))[1],
-            // 'iv_key'            =>  explode('container-', $response->filter("div[class*='wrapper container-']")->attr('class'))[1],
-            // 'decryption_key'    =>  explode('videocontent-', $response->filter("div[class*='videocontent-']")->attr('class'))[1],
+            'title' => urldecode(str_replace('+', ' ', $response->filter('#title')->attr('value'))),
+            'episode_id' => str_replace(' ', '-', $episode_id),
+            'video_id' => $response->filter('#id')->attr('value'),
+            'type' => $response->filter('#typesub')->attr('value'),
+            'download_uri' => "https://s3taku.com/download?id=$token",
         ]);
 
         $params = $ajax->generateAjaxParams(
@@ -168,11 +111,6 @@ class VidstreamVideoSpider extends BasicSpider
 
     public function parseVideoData(Response $response)
     {
-        // yield $this->item([
-        //     'uri' => $response->getUri(),
-        //     'options' => $response->getRequest()->getOptions(),
-        // ]);
-
         $ajax = new Ajax(
             config('app.decryption_key'),
             config('app.iv_key'),
