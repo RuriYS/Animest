a52866d9165b70eb941a124d842d01d9db01f634
ruriyoshinova <github@ruriyoshinova.dev>
2024-09-06 22:11:44 +0800

    🐞 fix: Routes

--- routes/api.php
+++ routes/api.php
@@ -4,16 +4,12 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\VidstreamVideoController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('/home')
-->group(function () {
-    Route::get('/', [HomeController::class, 'home'])->name('home.index');
-});
-
 Route::prefix('/spider')
 ->group(function () {
     Route::prefix('/vidstream')
     ->group(function () {
-        Route::get('{episode_id}', [VidstreamVideoController::class, 'get']);
-        Route::get('{episode_id}/store', [VidstreamVideoController::class, 'store']);
+                Route::get('{episode_id}', [VidstreamVideoController::class, 'index']);
+                Route::get('{episode_id}/get', [VidstreamVideoController::class, 'get']);
+                Route::get('{episode_id}/raw', [VidstreamVideoController::class, 'getRaw']);
     });
 });
