0bca56a538d9e7f7fe2a70f4451d46d96f4de693
ruriyoshinova <github@ruriyoshinova.dev>
2024-09-08 17:34:47 +0800

    🐞 fix: I forgot to remove it

--- resources/src/elements/Section.tsx
+++ resources/src/elements/Section.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import Button from './Button';
 
 const Section = () => {
-    return (
-        <>
-            <Button>This is a button lol</Button>
-        </>
-    );
+    return;
 };
 
 export default Section;
