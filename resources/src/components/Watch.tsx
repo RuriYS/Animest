b3e475d106566375fcd647299df49a71c5e0e8b0
ruriyoshinova <github@ruriyoshinova.dev>
2024-09-12 15:04:09 +0800

    ✨ feat: watch page component

--- resources/src/components/Watch.tsx
+++ resources/src/components/Watch.tsx
@@ -17,8 +17,10 @@ const WatchContainer = styled.div`
 `;
 
 interface Meta {
+    id: string;
     title: string;
-    date_added: string;
+    description: string;
+    episodes: number;
     splash: string;
 }
 
@@ -26,102 +28,137 @@ interface Source {
     file: string;
 }
 
-interface Video {
-    stream_data: {
-        source: Source[];
-        source_bk: Source[];
+interface Episode {
+    title_id: string;
+    episode_index: string;
+    upload_date: string;
+    video: {
+        stream_data: {
+            source: Source[];
+            source_bk: Source[];
+        };
     };
+    views: number;
 }
+
+const Moment: React.FC<{ uploadDate?: string }> = ({ uploadDate }) => {
+    const [dateNum, dateSuffix] = getMoment(uploadDate);
+    return (
+        <p className='flex gap-1 text-sm'>
+            <span>{dateNum}</span>
+            <span className='text-gray-300'>{dateSuffix}</span>
+        </p>
+    );
+};
+
+const getMoment = (uploadDate?: string): [string, string] => {
+    if (!uploadDate) return ['', ''];
+    const [dateNum, ...dateSuffixes] = moment(uploadDate).fromNow().split(' ');
+    const suffix = dateSuffixes.join(' ');
+    return [dateNum, suffix];
+};
+
+const EpisodeCard: React.FC<{
+    episode: Episode;
+    meta: Meta;
+}> = ({ episode, meta }) => (
+    <Episode to={`/watch/${meta.id}/episode/${episode.episode_index}`}>
+        <img src={meta.splash} alt={meta.id} />
+        <div>
+            <h1>{meta.title}</h1>
+            <p>Episode {episode.episode_index}</p>
+            <Moment uploadDate={episode.upload_date} />
+        </div>
+    </Episode>
+);
 
 const Watch: React.FC = () => {
     const { id, episodeId } = useParams<{ id: string; episodeId: string }>();
     const [loading, setLoading] = useState<boolean>(true);
-    const [meta, setMeta] = useState<Meta | null>();
-    const [video, setVideo] = useState<Video | null>();
+    const [meta, setMeta] = useState<Meta | null>(null);
+    const [episodes, setEpisodes] = useState<Episode[] | null>(null);
+    const [episode, setEpisode] = useState<Episode | undefined>(undefined);
+    const [views, setViews] = useState<number | undefined>(0);
 
     useEffect(() => {
-        const fetchStreamData = async () => {
-            try {
-                const { data } = await axios.get(
-                    `/api/videos/${id}/episode-${episodeId}`,
-                );
-                const { meta, video } = data;
-                setMeta(meta);
-                setVideo(video);
-                setLoading(false);
-            } catch (error) {
-                console.error('Failed to fetch stream data:', error);
-                setLoading(false);
-            }
+        const fetchData = async () => {
+            const { data } = await axios.get(`/api/videos/${id}`);
+            setEpisodes(
+                data.episodes.sort((a: Episode, b: Episode) =>
+                    moment(b.upload_date).diff(moment(a.upload_date)),
+                ),
+            );
+            setLoading(false);
         };
+        fetchData();
+    }, []);
 
-        fetchStreamData();
-    }, [id, episodeId]);
+    useEffect(() => {
+        const getMeta = async () => {
+            const anime = await axios.get(`/api/animes/${id}`);
+            setMeta(anime.data);
+        };
+        if (episodes) {
+            getMeta();
+            setEpisode(episodes.find((ep) => ep.episode_index === episodeId));
+        }
+    }, [episodes, episodeId]);
 
-    const date = meta?.date_added.toString();
-    const [dateNum, ...dateSuffixes] = moment(date).fromNow().split(' ');
+    if (loading) return <div></div>;
 
     return (
         <Constraint>
             <WatchContainer>
-                <MediaPlayer
-                    title={meta?.title}
-                    src={video?.stream_data.source[0].file}
-                    playsInline
-                >
-                    <MediaProvider />
-                    <PlyrLayout icons={plyrLayoutIcons} />
-                </MediaPlayer>
-                <div className='grid px-2 gap-2 lg:grid-cols-2 lg:px-8 lg:gap-8'>
-                    <div className='flex flex-col gap-y-4'>
-                        <h1 className='text-2xl font-semibold'>
-                            {meta?.title}
-                        </h1>
-                        <div className='hidden flex-1 lg:flex gap-4 p-4 bg-neutral-900 rounded-lg'>
-                            <p className='flex gap-1 text-sm'>
-                                <span>69420</span>
-                                <span className='text-gray-300'>views</span>
-                            </p>
-                            <p className='flex gap-1 text-sm'>
-                                <span>{dateNum}</span>
-                                <span className='text-gray-300'>
-                                    {dateSuffixes.join(' ')}
-                                </span>
-                            </p>
-                        </div>
-                    </div>
-                    <div className='flex flex-col gap-y-4 p-4 bg-neutral-900 rounded-lg'>
-                        <div className='lg:hidden flex gap-4 bg-neutral-900 rounded-lg'>
-                            <p className='flex gap-1 text-sm'>
-                                <span>69420</span>
-                                <span className='text-gray-300'>views</span>
-                            </p>
-                            <p className='flex gap-1 text-sm'>
-                                <span>{dateNum}</span>
-                                <span className='text-gray-300'>
-                                    {dateSuffixes.join(' ')}
-                                </span>
-                            </p>
-                        </div>
-                        <h1 className='text-lg font-semibold'>Episodes</h1>
-                        <ul>
-                            <Episode
-                                to={
-                                    '/watch/shikanoko-nokonoko-koshitantan/episode/1'
-                                }
-                            >
-                                <img
-                                    src='https://gogocdn.net/cover/shikanoko-nokonoko-koshitantan.png'
-                                    alt='Shikanoko Nokonoko Koshitantan - Episode 1'
-                                />
-                                <div className='flex flex-col gap-y-2'>
-                                    <p>Episode 1</p>
-                                    <span>2 months ago</span>
+                {episode && (
+                    <MediaPlayer
+                        title={meta?.title}
+                        src={episode?.video.stream_data.source[0].file}
+                        playsInline
+                    >
+                        <MediaProvider />
+                        <PlyrLayout icons={plyrLayoutIcons} />
+                    </MediaPlayer>
+                )}
+                {meta && (
+                    <div className='grid px-2 gap-2 lg:grid-cols-2 lg:px-8 lg:gap-8'>
+                        <div className='flex flex-col gap-y-4'>
+                            <div>
+                                <h1 className='text-2xl font-semibold'>
+                                    {meta.title}
+                                </h1>
+                                <h2 className='font-semibold'>
+                                    Episode {episode?.episode_index}
+                                </h2>
+                            </div>
+                            <div className='flex-1 flex-col lg:flex gap-4 p-4 bg-neutral-900 rounded-lg'>
+                                <div className='flex gap-4'>
+                                    <p className='flex gap-1 text-sm'>
+                                        <span>{views}</span>
+                                        <span className='text-gray-300'>
+                                            views
+                                        </span>
+                                    </p>
+                                    <Moment uploadDate={episode?.upload_date} />
                                 </div>
-                            </Episode>
-                        </ul>
+                                <p className='text-xs text-gray-500'>
+                                    {meta.description}
+                                </p>
+                            </div>
+                        </div>
+                        <div className='flex flex-col gap-y-4 p-4 bg-neutral-900 rounded-lg'>
+                            <h1 className='text-lg font-semibold'>Episodes</h1>
+                            <ul>
+                                {episodes?.map((episode) => (
+                                    <EpisodeCard
+                                        key={`${episode.title_id}-${episode.episode_index}`}
+                                        episode={episode}
+                                        meta={meta}
+                                    />
+                                ))}
+                            </ul>
+                        </div>
                     </div>
-                </div>
+                )}
             </WatchContainer>
         </Constraint>
     );
