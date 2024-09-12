346b6542a44175b28fbedaadf1dad7ed1119da50
ruriyoshinova <github@ruriyoshinova.dev>
2024-09-12 15:59:46 +0800

    🎉 init: Page

--- resources/src/components/Anime.tsx
+++ resources/src/components/Anime.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import Constraint from '../elements/Constraint';
 
-const Anime = () => (
-    <Constraint>
-        <h1>Title here</h1>
-        <h1>Description</h1>
-    </Constraint>
-);
+const Anime = () => <Constraint></Constraint>;
 
 export default Anime;
