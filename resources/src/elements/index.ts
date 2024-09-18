0e258e4d3736ef25b0b8c33984b3ac2b29177115
ruriyoshinova <github@ruriyoshinova.dev>
2024-09-18 19:44:24 +0800

    🎈 perf: This is not optimization

---
+++ resources/src/elements/index.ts
@@ -0,0 +1,13 @@
+export { default as Constraint } from './Constraint';
+export { default as ContentContainer } from './ContentContainer';
+export { default as Episode } from './Episode';
+export { default as EpisodeCard } from './EpisodeCard';
+export { default as EpisodeInfo } from './EpisodeInfo';
+export { default as EpisodeList } from './EpisodeList';
+export { default as MainContainer } from './MainContainer';
+export { default as Moment } from './Moment';
+export { default as Pagination } from './Pagination';
+export { default as PlayerWrapper } from './PlayerWrapper';
+export { default as SortSelect } from './SortSelect';
+export { default as SearchResult } from './SearchResult';
+export { default as WatchLoading } from './WatchLoading';
