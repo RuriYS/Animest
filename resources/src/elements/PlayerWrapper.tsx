b28c59a2eac7c939d1426b099188f50d4bc2c74d
ruriyoshinova <github@ruriyoshinova.dev>
2024-09-19 05:29:28 +0800

    ✨ feat: use proxied URL

--- resources/src/elements/PlayerWrapper.tsx
+++ resources/src/elements/PlayerWrapper.tsx
@@ -1,18 +1,65 @@
 import React, { useEffect, useRef } from 'react';
 import {
+    isHLSProvider,
     MediaPlayer,
     MediaPlayerInstance,
     MediaProvider,
+    MediaProviderAdapter,
+    MediaProviderChangeEvent,
 } from '@vidstack/react';
+import Hls, {
+    HlsConfig,
+    LoaderCallbacks,
+    LoaderConfiguration,
+    LoaderContext,
+} from 'hls.js';
 import {
-    PlyrLayout,
-    plyrLayoutIcons,
-} from '@vidstack/react/player/layouts/plyr';
+    defaultLayoutIcons,
+    DefaultVideoLayout,
+} from '@vidstack/react/player/layouts/default';
+import '@vidstack/react/player/styles/default/theme.css';
+import '@vidstack/react/player/styles/default/layouts/video.css';
 
 interface MediaPlayerWrapperProps {
     src: string;
     title: string;
     onEnd: () => void;
+}
+
+const config: Partial<HlsConfig> = {
+    autoStartLoad: true,
+    debug: true,
+    maxBufferLength: 5,
+    maxMaxBufferLength: 5,
+    maxBufferSize: 1 * 1000 * 1000,
+    maxBufferHole: 0.5,
+    lowLatencyMode: true,
+    highBufferWatchdogPeriod: 1,
+    nudgeOffset: 0.1,
+    nudgeMaxRetry: 5,
+    maxFragLookUpTolerance: 0.1,
+    liveSyncDurationCount: 1,
+    liveMaxLatencyDurationCount: 2,
+    fragLoadingTimeOut: 20000,
+    fragLoadingMaxRetry: 2,
+    fragLoadingRetryDelay: 1000,
+    fragLoadingMaxRetryTimeout: 5000,
+    startFragPrefetch: false,
+    progressive: false,
+    abrEwmaDefaultEstimate: 500000,
+    abrEwmaFastLive: 3,
+    abrEwmaSlowLive: 9,
+};
+
+function onProviderChange(
+    provider: MediaProviderAdapter | null,
+    nativeEvent: MediaProviderChangeEvent,
+) {
+    if (isHLSProvider(provider)) {
+        console.info('Player', 'Changed HLS Provider');
+        provider.library = Hls;
+        provider.config = config;
+    }
 }
 
 const PlayerWrapper: React.FC<MediaPlayerWrapperProps> = ({
@@ -32,16 +79,19 @@ const PlayerWrapper: React.FC<MediaPlayerWrapperProps> = ({
         }
     }, [onEnd]);
 
+    const uri = new URL(src);
+
     return (
         <MediaPlayer
             ref={playerRef}
             onEnd={onEnd}
             title={title}
-            src={src}
+            src={`/api/proxy/${uri.host}${uri.pathname}`}
             playsInline
+            onProviderChange={onProviderChange}
         >
             <MediaProvider />
-            <PlyrLayout icons={plyrLayoutIcons} />
+            <DefaultVideoLayout icons={defaultLayoutIcons} />
         </MediaPlayer>
     );
 };
