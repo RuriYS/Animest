32e12c87f214f33b46a5d516fd1ca60d49286e62
ruriyoshinova <github@ruriyoshinova.dev>
2024-09-22 04:55:58 +0800

    ✨ feat: 404 page

--- resources/src/elements/ForOhFour.tsx
+++ resources/src/elements/ForOhFour.tsx
@@ -3,12 +3,14 @@ import { useNavigate } from 'react-router-dom';
 import ContentContainer from './ContentContainer';
 import Constraint from './Constraint';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const RedirectToRickroll = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        window.location.href = '/videos/idiot.mp4';
+        if (window.location.href.includes('admin'))
+            window.location.href = '/videos/idiot.mp4';
     }, [navigate]);
 
     return null;
@@ -18,7 +20,12 @@ export default function Component() {
     return (
         <Constraint>
             <ContentContainer>
-                <h1 className='text-xl'>404 not found</h1>
+                <div className='flex flex-col gap-4 mx-auto text-center'>
+                    <h1 className='text-2xl'>404 | Are you lost?</h1>
+                    <Link to={'/home'}>
+                        <p className='text-lg underline'>Take me home.</p>
+                    </Link>
+                </div>
                 <RedirectToRickroll />
             </ContentContainer>
         </Constraint>
